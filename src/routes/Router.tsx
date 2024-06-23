769aa2b1a81c6f6441978a1e9f8242cbf451fb91
류경민 <ryu@lyugyeongmin-ui-MacBookAir.local>
2024-06-23 18:29:04 +0900

    :wrench: Config: firebase, router 설치 및 연동
    firebase, router 설치 및 연동

---
+++ src/routes/Router.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "@/pages/Common/Home";
+import Login from "@/pages/Common/Login";
+import SignUp from "@/pages/Common/SignUp";
+import ProductList from "@/pages/Common/ProductList";
+import ProductDetail from "@/pages/Common/ProductDetail";
+import Cart from "@/pages/Common/Cart";
+import MyPage from "@/pages/User/MyPage";
+import ProfileEdit from "@/pages/User/ProfileEdit";
+import OrderHistory from "@/pages/User/OrderHistory";
+import CancelHistory from "@/pages/User/CancelHistory";
+import Orders from "@/pages/Admin/Orders";
+import Inventory from "@/pages/Admin/Inventory";
+import CreateProduct from "@/pages/Admin/CreateProduct";
+import Error from "@/pages/Common/Error";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/category" element={<ProductList />} />
+        <Route path="/product" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/profile" element={<ProfileEdit />} />
+        <Route path="/history" element={<OrderHistory />} />
+        <Route path="/cancel" element={<CancelHistory />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/create" element={<CreateProduct />} />
+        <Route path="/cancel" element={<CancelHistory />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
