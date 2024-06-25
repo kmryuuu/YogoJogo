ab3d060a6d44130490b4fbe5776b9912cca21a52
kmryuuu <jjpig92@naver.com>
2024-06-25 15:43:52 +0900

    :sparkles: Feat: 로그인 기능 구현
    로그인 기능 구현

--- src/pages/Common/Login.tsx
+++ src/pages/Common/Login.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import LoginForm from "@/components/Form/LoginForm";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="justify-content flex flex-col items-center">
+      <LoginForm />
+      <div className="flex gap-2">
+        <p>아직 회원이 아니신가요?</p>
+        <Link to="/signup" className="underline">
+          회원가입
+        </Link>
+      </div>
+      <button type="button" className="bg-buttonKakao">
+        카카오로 로그인
+      </button>
+    </div>
+  );
 };
 
 export default Login;
