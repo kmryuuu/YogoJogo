d5c844c8504fd0de159d3915e58514b4c7d3b6b9
kmryuuu <jjpig92@naver.com>
2024-06-26 23:05:39 +0900

    :sparkles: Feat: 회원가입, 로그아웃 기능 구현
    회원가입, 로그아웃 기능 구현

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "@/firebase";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  User,
 } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 interface AuthError {
   code: string;
@@ -14,11 +18,12 @@ const errorMessage = (code: string) => {
   switch (code) {
     case "auth/user-not-found":
     case "auth/wrong-password":
-    case "auth/email-already-in-use":
     case "auth/invalid-email":
     case "auth/invalid-credential":
     case "auth/weak-password":
       return "이메일 혹은 비밀번호가 일치하지 않습니다.";
+    case "auth/email-already-in-use":
+      return "이미 사용 중인 이메일입니다.";
     case "auth/network-request-failed":
       return "네트워크 연결에 실패 하였습니다.";
     case "auth/internal-error":
@@ -30,15 +35,36 @@ const errorMessage = (code: string) => {
 
 const useAuth = () => {
   const [error, setError] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
+
+  // 현재 사용자 업데이트
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // 오류 초기화 함수
+  const resetError = () => {
+    setError(null);
+  };
 
   // 회원가입
   const signup = async (email: string, password: string) => {
     console.log("회원가입 정보", email, password);
-    await createUserWithEmailAndPassword(auth, email, password);
     try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/");
     } catch (error) {
       const authError = error as AuthError;
-      setError(errorMessage(authError.code));
+      if (authError.code === "auth/email-already-in-use") {
+        return "duplicate email";
+      } else {
+        setError(errorMessage(authError.code));
+      }
     }
   };
 
@@ -47,13 +73,26 @@ const useAuth = () => {
     console.log("들어온 이메일, 비밀번호", email, password);
     try {
       await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+      setError(null);
     } catch (error) {
       const authError = error as AuthError;
       setError(errorMessage(authError.code));
     }
   };
 
-  return { signup, login, error };
+  // 로그아웃
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+      setUser(null);
+    } catch (error) {
+      console.log("로그아웃을 하지 못했습니다.");
+    }
+  };
+
+  return { signup, login, logout, user, error, resetError };
 };
 
 export default useAuth;
--- src/pages/Common/SignUp.tsx
+++ src/pages/Common/SignUp.tsx
@@ -1,5 +1,12 @@
+import SignUpForm from "@/components/Form/SignUpForm";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="justify-content flex flex-col items-center">
+      <h2 className="text-2xl">회원가입</h2>
+      <SignUpForm />
+    </div>
+  );
 };
 
 export default SignUp;
