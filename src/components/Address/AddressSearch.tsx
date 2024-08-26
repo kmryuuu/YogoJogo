ff720f06e66b8b576a3ee0cccde6bedce786322d
kmryuuu <jjpig92@naver.com>
2024-08-26 15:05:44 +0900

    :art: Style: 주소 검색 모달 UI 수정
    주소 검색 모달 UI 수정

--- src/components/Address/AddressSearch.tsx
+++ src/components/Address/AddressSearch.tsx
@@ -12,17 +12,16 @@ const AddressSearch = ({ isOpen, onClose, onComplete }: AddressSearchProps) => {
       isOpen={isOpen}
       onRequestClose={onClose}
       overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-40"
-      className="modal-content"
+      className="modal-content relative h-[500px] max-w-lg"
     >
-      <div className="relative h-[500px] w-full max-w-md rounded-lg bg-white p-4">
-        <button
-          onClick={onClose}
-          className="absolute right-4 top-6 text-xl font-bold"
-        >
-          X
-        </button>
-        <DaumPostcode onComplete={onComplete} className="mt-10" />
-      </div>
+      <p className="text-xl font-bold">주소 검색</p>
+      <button
+        onClick={onClose}
+        className="absolute right-6 top-6 text-xl font-bold"
+      >
+        X
+      </button>
+      <DaumPostcode onComplete={onComplete} className="mt-5" />
     </Modal>
   );
 };
